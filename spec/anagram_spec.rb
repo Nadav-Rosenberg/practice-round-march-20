6e1702c0c5777be18b4eea6ff24daddfb385114c
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-03-20 12:49:49 +0000

    trail

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,1 +1,11 @@
-#Your code goes here
+require 'anagram.rb'
+
+describe 'anagram finder' do
+
+
+  let(:anagram) {Anagram.new('cat', ["try","how","eat","far","act","see","hit","lie","key","tac","sit","its","tea"])}
+
+  it 'should intialize with the word' do
+    expect(anagram.word).to eq 'cat'
+  end
+end
